68ea426d973584802271bcd167e37049e130d99a
KITRI <soongon@gmail.com>
2024-07-08 14:01:01 +0900

    initial commit

---
+++ src/main/java/kr/re/kitri/devopsdemoapp/controller/HomeController.java
@@ -0,0 +1,18 @@
+package kr.re.kitri.devopsdemoapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "안녕하세요";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "김순곤 입니다.";
+    }
+}
---
+++ src/test/java/kr/re/kitri/devopsdemoapp/DevopsDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.re.kitri.devopsdemoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsDemoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
