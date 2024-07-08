45b060e11610e6565df0dbb0f94ed64a3d1cbc90
soongonkim <soongon@gmail.com>
2024-07-09 00:14:07 +0900

    컨텍스트 경로 추가

--- src/main/java/kr/re/kitri/devopsdemoapp/controller/HomeController.java
+++ src/main/java/kr/re/kitri/devopsdemoapp/controller/HomeController.java
@@ -1,9 +1,11 @@
 package kr.re.kitri.devopsdemoapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/myapp")
 public class HomeController {
 
     @GetMapping("/")
