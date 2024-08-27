3936e62a8282120b26da56cc17decc76b01c980f
soongon <soongon@gmail.com>
2024-08-27 14:10:21 +0900

    웹훅 테스트

--- src/main/java/kr/re/kitri/devopsdemoapp/controller/HomeController.java
+++ src/main/java/kr/re/kitri/devopsdemoapp/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "웹훅으로 트리거링.. ";
+        return "웹훅으로 트리거링.. ㄴㄴㄴ";
     }
 
     @GetMapping("/about")
